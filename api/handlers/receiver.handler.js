8a37b919c5b92be7ce9dee1630c58a49b0a003be
PRESTATIONLIVEE\prestation <teste.antoine@free.fr>
2017-07-07 15:58:29 +0200

    modification(receiver.handler.js syntax)

--- api/handlers/receiver.handler.js
+++ api/handlers/receiver.handler.js
@@ -2,19 +2,37 @@
 
 const http = require('http');
 const express = require('express');
-const app = express();
 const twilio = require('twilio');
-
+const MessagingResponse = require('twilio').twiml.MessagingResponse;
+const bodyParser = require('body-parser');
 
 module.exports = (rMqService) => {
   return {
     receiving: (request, reply) => {
+      // Nouvelle instance de message
+      const twiml = new MessagingResponse();
+
+
+      // twiml.message('The Robots are coming! Head for the hills!');
+      // reply.writeHead(200, { 'Content-Type': 'text/xml' });
+      console.log('payload twilio:', request.payload.Body, request.payload.From);
+      // reply(twiml.toString());
+
+
+      // Reponse vide
+      reply().code(200);
+
+      // Envoi du message dans la queue
       const msg = request.payload;
       rMqService.sendMessage(msg);
-      reply('the message have been received');
+
+
     }
   }
 }
+
+
+
 
 
 
