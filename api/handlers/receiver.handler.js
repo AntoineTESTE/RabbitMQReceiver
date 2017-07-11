df870b6fa0740f5c68bd2139574d3a17edf2419e
PRESTATIONLIVEE\prestation <teste.antoine@free.fr>
2017-07-11 15:12:37 +0200

    add(conventions)

--- api/handlers/receiver.handler.js
+++ api/handlers/receiver.handler.js
@@ -1,7 +1,5 @@
 'use strict';
 
-const http = require('http');
-const express = require('express');
 const twilio = require('twilio');
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
 const bodyParser = require('body-parser');
@@ -9,27 +7,23 @@ const bodyParser = require('body-parser');
 module.exports = (rMqService) => {
   return {
     receiving: (request, reply) => {
+
       // Nouvelle instance de message
       const twiml = new MessagingResponse();
-      // twiml.message('The Robots are coming! Head for the hills!');
-      // reply.writeHead(200, { 'Content-Type': 'text/xml' });
 
-      // reply(twiml.toString());
-      // Reponse vide
-      reply().code(200);
+      twiml.message('your message have been received');
+      //reply.writeHead(200, { 'Content-Type': 'text/xml' });
+      reply(twiml.toString()).code(200);
 
-      function msgmodel(content, sender, type, provider, providerMessageId) {
-        this.content = "blabla";
-        this.sender = "boby";
-        this.type = "sms";
-        this.provider = "twilio";
-        this.providerMessageId = "EE4DfgAd75175eZZzF";
-      };
-
-      const msg = new msgmodel()
+      const msg = {
+        content: request.payload.Body,
+        sender: request.payload.From,
+        type: "SMS",
+        provider: "TWILIO",
+        providerMessageId: request.payload.SmsMessageSid
+      }
 
       console.log('payload twilio:', msg);
-
 
       // Envoi du message dans la queue
       rMqService.sendMessage(msg);
@@ -38,24 +32,3 @@ module.exports = (rMqService) => {
     }
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-2
--- api/routes/receiver.routes.js
+++ api/routes/receiver.routes.js
@@ -7,7 +7,7 @@ module.exports = (handlers, services, server) => {
   //Add the route to receive the message
   server.route({
     method: 'POST',
-    path: '/message', //labs.livee.com:3000/twilio/webhook <- url publique accessible par twilio
+    path: '/message',                          //'/message', //labs.livee.com:3000/twilio/webhook <- url publique accessible par twilio
     config: {
       description: '/message',
       notes: ['api'],
